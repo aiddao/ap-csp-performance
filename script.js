1557369279c3851b4e0864203d35aa52ceb5a8b9
aiddao <153690256+aiddao@users.noreply.github.com>
2024-03-20 01:20:13 +0000

    Completed the hint and win condition algorithm

--- script.js
+++ script.js
@@ -3,21 +3,25 @@ const GENBUT = document.getElementById("genNumber");
 let hint = document.getElementsByClassName("hint-box")[0];
 let guess;
 let numGuess;
+let score;
 
+//if a number exists from a previous session, the number will remain the same
 if(localStorage.numHolder){
-    numGuess = localStorage.numHolder;
+    numGuess = parseInt(localStorage.numHolder);
 }
 
+//generates number from 1 to the given range
 GENBUT.addEventListener("click", function(){
     let num=Math.floor(Math.random()* (RANGE - 1) + 1);
     numGuess = num;
     localStorage.numHolder = numGuess;
 });
 
+//turns the user's guess into a variable and checks if they won or not
 function userInput(){
     event.preventDefault();
     let formData = $("form").serializeArray();
-    guess = formData[0].value;
+    guess = parseInt(formData[0].value);
 
     if (guess == numGuess){
         hint.innerText = "YOU WIN!!!!!";
@@ -26,14 +30,15 @@ function userInput(){
     }
 }
 
+//gives hints based on if the user's distance from the answer in percentages
 function checkGuess(){
-    if(guess < numGuess){
+    if (guess < numGuess){
         let numOff = numGuess - guess;
         if(numOff <= RANGE * .10){
             hint.innerText = "Very close. A little higher!";
         }else if (numOff > RANGE * .10 && numOff < RANGE * .25) {
             hint.innerText = "You're getting there! Higher!"
-        }else if (numOff > RANGE * .25 && numOff < RANGE * .5) {
+        }else if (numOff >= RANGE * .25 && numOff < RANGE * .50) {
             hint.innerText = "Higher!";
         }else{
             hint.innerText = "You're quite a bit low...";
@@ -44,7 +49,7 @@ function checkGuess(){
             hint.innerText = "Very close. A little lower!";
         }else if (numOff > RANGE * .10 && numOff < RANGE * .25) {
             hint.innerText = "You're getting there! Lower!"
-        }else if (numOff > RANGE * .25 && numOff < RANGE * .5) {
+        }else if (numOff >= RANGE * .25 && numOff < RANGE * .50) {
             hint.innerText = "Lower!";
         }else{
             hint.innerText = "You're quite a bit high...";
