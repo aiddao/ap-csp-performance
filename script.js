f5835e9916e8aed663899fecc33070d3baf65e58
aiddao <153690256+aiddao@users.noreply.github.com>
2024-04-05 21:11:50 +0000

    Yes

--- script.js
+++ script.js
@@ -57,6 +57,34 @@ function userInput(){
     hint = document.createElement("div");
     hint.setAttribute("style", "color")
     HINTBOX.insertBefore(hint, HINTBOX.firstChild);
+    winCondition(guess, numGuess, scoreList, score, num);
+}
+//checks how far off the guess is from the answer
+function checkCondition(a, b, c, d, e, num){
+    if(e <= RANGE * .10){
+        hint.innerText = num + ". "+ a;
+    }else if (e > RANGE * .10 && e < RANGE * .25) {
+        hint.innerText = num + ". "+ b;
+    }else if (e >= RANGE * .25 && e < RANGE * .50) {
+        hint.innerText = num + ". "+ c;
+    }else{
+        hint.innerText = num + ". "+ d;
+    }
+}
+
+//gives hint messages based on if the user is higher or lower than the answer
+function checkGuess(li){
+    if (guess < numGuess){
+        let numOff = numGuess - guess;
+        checkCondition("Very close. A little higher!", "You're getting there! Higher!", "Higher!", "You're quite a bit low...", numOff, li);
+    }else{
+        let numOff = guess - numGuess;
+        checkCondition("Very close. A little lower!", "You're getting there! Lower!", "Lower!", "You're quite a bit high...", numOff, li);
+    }
+}
+
+//Put in parameters
+function winCondition(a, b, c, d, e){
     if (guess == numGuess){
         hint.innerText = "YOU WIN!!!!!";
         if(scoreList < score){
@@ -81,28 +109,5 @@ function userInput(){
         let hintz = HINTBOX.firstElementChild;
         hintzList.push(hintz.innerText);
         localStorage.setItem("hintz", JSON.stringify(hintzList));
-    }
-}
-//checks how far off the guess is from the answer
-function checkCondition(a, b, c, d, e, num){
-    if(e <= RANGE * .10){
-        hint.innerText = num + ". "+ a;
-    }else if (e > RANGE * .10 && e < RANGE * .25) {
-        hint.innerText = num + ". "+ b;
-    }else if (e >= RANGE * .25 && e < RANGE * .50) {
-        hint.innerText = num + ". "+ c;
-    }else{
-        hint.innerText = num + ". "+ d;
-    }
-}
-
-//gives hint messages based on if the user is higher or lower than the answer
-function checkGuess(li){
-    if (guess < numGuess){
-        let numOff = numGuess - guess;
-        checkCondition("Very close. A little higher!", "You're getting there! Higher!", "Higher!", "You're quite a bit low...", numOff, li);
-    }else{
-        let numOff = guess - numGuess;
-        checkCondition("Very close. A little lower!", "You're getting there! Lower!", "Lower!", "You're quite a bit high...", numOff, li);
     }
 }
