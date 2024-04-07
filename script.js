0e3de4ea26ba38fab2b8893ddfba6f01ab7bfcf6
aiddao <153690256+aiddao@users.noreply.github.com>
2024-04-07 00:58:25 +0000

    Co-authored-by: Arc-A770 <Arc-A770@users.noreply.github.com>

--- script.js
+++ script.js
@@ -36,6 +36,20 @@ if(localStorage.numHolder){
     numGuess = parseInt(localStorage.numHolder);
 }
 
+if(localStorage.color){
+    let data = JSON.parse(localStorage.color);
+    console.log(data);
+    document.body.setAttribute("style", "background-color:" + data[0].value + "; color:" + data[1].value + ";");
+}
+
+function customize(){
+    event.preventDefault();
+    let formData = $("form").serializeArray();
+    document.body.setAttribute("style", "background-color:" + formData[0].value + "; color:" + formData[1].value + ";");
+    localStorage.setItem("color", JSON.stringify(formData));
+    console.log(formData);
+}
+
 //generates number from 1 to the given range
 GENBUT.addEventListener("click", function(){
     let num=Math.floor(Math.random()* (RANGE - 1) + 1);
@@ -47,17 +61,21 @@ GENBUT.addEventListener("click", function(){
 //turns the user's guess into a variable and checks if they won or not
 function userInput(){
     event.preventDefault();
+    if(localStorage.numHolder){
     let num = HINTBOX.childElementCount;
     scoreBox.innerText = "Current Score: " + num;
     localStorage.setItem("scorez", JSON.stringify(num));
     let formData = $("form").serializeArray();
-    if (formData[0].value == ""){
-        formData[0].value = 0;
+    if (formData[2].value == ""){
+        formData[2].value = 0;
     }
-    guess = parseInt(formData[0].value);
+    guess = parseInt(formData[2].value);
     hint = document.createElement("div");
     HINTBOX.insertBefore(hint, HINTBOX.firstChild);
     winCondition(guess, numGuess, scoreList, num);
+} else {
+    alert("You have no number to guess");
+}
 }
 //checks how far off the guess is from the answer
 function checkCondition(a, b, c, d, e, num){
