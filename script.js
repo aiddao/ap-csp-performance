907c1c4acd3170b2dbd76a3bb7ef7f90e96fe36f
aiddao <153690256+aiddao@users.noreply.github.com>
2024-04-12 00:35:25 +0000

    Optimizations. Made parameters easier to read.

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ let guess;
 let numGuess;
 let scoreBox = document.getElementById("score-box");
 let high = document.getElementById("high-score");
+let gameForm = document.getElementById("game-form");
 let hintzList = [];
 let scoreList;
 let highScore = "";
+let num;
 let hint;
 let time;
+let timeScore = 0;
 
 if(localStorage.hintz){
     hintzList = JSON.parse(localStorage.hintz);
@@ -22,9 +25,6 @@ if(localStorage.hintz){
 if(localStorage.scorez){
     score = JSON.parse(localStorage.scorez);
     scoreBox.innerHTML = "Previous Score: " + score;
-}
-if(localStorage.scoreWin){
-    scoreList = JSON.parse(localStorage.scoreWin);
 }
 
 if(localStorage.highScore){
@@ -58,19 +58,21 @@ GENBUT.addEventListener("click", function(){
     if(localStorage.numHolder){
         alert("There is already a generated number!!");
     } else {
-    let num=Math.floor(Math.random()* (RANGE - 1) + 1);
-    numGuess = num;
+    let number=Math.floor(Math.random()* (RANGE - 1) + 1);
+    numGuess = number;
     localStorage.numHolder = numGuess;
     alert("Number Generated");
     }
+    time = setInterval(function(){
 
+    }, 10000)
 });
 
 //turns the user's guess into a variable and checks if they won or not
 function userInput(){
     event.preventDefault();
     if(localStorage.numHolder){
-    let num = HINTBOX.childElementCount;
+    num = HINTBOX.childElementCount;
     scoreBox.innerText = "Current Score: " + num;
     localStorage.setItem("scorez", JSON.stringify(num));
     let formData = $("form").serializeArray();
@@ -80,54 +82,48 @@ function userInput(){
     guess = parseInt(formData[2].value);
     hint = document.createElement("div");
     HINTBOX.insertBefore(hint, HINTBOX.firstChild);
-    winCondition(guess, numGuess, scoreList, num);
+    winCondition(guess, numGuess, num);
 } else {
     alert("You have no number to guess");
 }
 }
 //checks how far off the guess is from the answer
-function checkCondition(a, b, c, d, e, num, g){
-    if(e <= RANGE * .10){
-        hint.innerText = num + ". "+ a + "["+g+"]";
-    }else if (e > RANGE * .10 && e < RANGE * .25) {
-        hint.innerText = num + ". "+ b + "["+g+"]";
-    }else if (e >= RANGE * .25 && e < RANGE * .50) {
-        hint.innerText = num + ". "+ c + "["+g+"]";
+function checkCondition(closeMsg, almostMsg, midMsg, wayOffMsg, amtOff){
+    if(amtOff <= RANGE * .10){
+        hint.innerText = num + ". "+ closeMsg + "["+guess+"]";
+    }else if (amtOff > RANGE * .10 && amtOff < RANGE * .25) {
+        hint.innerText = num + ". "+ almostMsg + "["+guess+"]";
+    }else if (amtOff >= RANGE * .25 && amtOff < RANGE * .50) {
+        hint.innerText = num + ". "+ midMsg + "["+guess+"]";
     }else{
-        hint.innerText = num + ". "+ d + "["+g+"]";
+        hint.innerText = num + ". "+ wayOffMsg + "["+guess+"]";
     }
 }
 
 //gives hint messages based on if the user is higher or lower than the answer
-function checkGuess(li, guess){
+function checkGuess(){
     let numOff = Math.abs(numGuess - guess);
     if (guess < numGuess){
-        checkCondition("Very close. A little higher! ", "You're getting there! Higher! ", "Higher! ", "You're quite a bit low ", numOff, li, guess);
+        checkCondition("Very close. A little higher! ", "You're getting there! Higher! ", "Higher! ", "You're quite a bit low ", numOff);
     }else{
-        checkCondition("Very close. A little lower! ", "You're getting there! Lower! ", "Lower! ", "You're quite a bit high ", numOff, li, guess);
+        checkCondition("Very close. A little lower! ", "You're getting there! Lower! ", "Lower! ", "You're quite a bit high ", numOff);
     }
 }
 
-function winCondition(a, b, c, d){
-    if (a == b){
-        alert("YOU WIN!!! Answer: "+numGuess+" | Score: " + d);
-        if(c < d){
-            c = d;
-            localStorage.setItem("scoreWin", JSON.stringify(c));
-        }
+function winCondition(userGuess, winValue, curScore){
+    if (userGuess == winValue){
+        alert("YOU WIN!!! Answer: "+winValue+" | Score: " + curScore);
         localStorage.removeItem("hintz");
         localStorage.removeItem("numHolder");
         if(highScore == ""){
             highScore = 0;
-            localStorage.setItem("highScore", JSON.stringify(d));
-            high.innerText = d;
-        }else if(highScore > d){
-            localStorage.setItem("highScore", JSON.stringify(d));
-            high.innerText = d;
+            localStorage.setItem("highScore", JSON.stringify(curScore));
+        }else if(highScore > curScore){
+            localStorage.setItem("highScore", JSON.stringify(curScore));
         }
         location.reload();
     }else{
-        checkGuess(d, a);
+        checkGuess();
         let hintz = HINTBOX.firstElementChild;
         hintzList.push(hintz.innerText);
         localStorage.setItem("hintz", JSON.stringify(hintzList));
